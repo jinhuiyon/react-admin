51e221d85652d4ed9faa21662eba589044dbdff9
jinhuiyong <2424960424@qq.com>
2019-06-21 20:55:02 +0800

    登录界面结构

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-// import './assets/less/index.less';
+import './assets/less/index.less';
 ReactDom.render(<Router><App/></Router>,document.getElementById('root'));
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,32 @@
 import React,{Component} from 'react';
+import { Form, Icon, Input, Button} from 'antd';
+import logo from './logo.png';
+import './index.less';
+const Item = Form.Item;
 export default class Login extends Component{
     render(){
-        return <div>
-            Login
-        </div>
+        return (
+           <div className="login">
+               <header className="login-header">
+                   <img src={logo} alt="logo"/>
+                   <h1>React项目: 后台管理系统</h1>
+               </header>
+               <section className="login-content">
+                   <h2>用户登录</h2>
+                   <Form className="login-form">
+                       <Item>
+                           <Input className="login-input" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入用户名"/>
+                       </Item>
+                       <Item>
+                           <Input className="login-input" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入密码"/>
+                       </Item>
+                       <Item>
+                           <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+                       </Item>
+                   </Form>
+               </section>
+           </div>
+        )
+        
     }
 }
