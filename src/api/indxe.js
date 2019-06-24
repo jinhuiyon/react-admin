3df5f1adf18d01e1593daee5ef2b1d5b7371976b
jinhuiyong <2424960424@qq.com>
2019-06-24 19:09:16 +0800

    内容头部效果

--- src/api/indxe.js
+++ src/api/indxe.js
@@ -1,3 +1,23 @@
 import ajax from './ajax';
-
+import { message } from 'antd';
+import jsonp from 'jsonp'
+// 登录请求
 export const reqLogin = (username,password) => ajax('/login',{username,password},'POST');
+
+// 天气请求
+export const reqWeather = function () {
+    return new Promise((resolve,reject) => {
+        jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`,{},function(err,data){
+            if(!err) {
+                const { dayPictureUrl, weather} = data.results[0].weather_data[0];
+                resolve({
+                    weatherImg:dayPictureUrl,
+                    weather
+                })
+            } else {
+                message.error('请求天气失败');
+                resolve();
+            }
+        })
+    })
+}
--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import MyButton from "../my-button";
 import { withRouter } from 'react-router-dom';
-import { Modal, Button } from 'antd';
+import { Modal } from 'antd';
 import { getItem, removeItem } from '../../utils/storage-tools';
-import logo from "../../assets/img/logo.png";
+import { reqWeather } from '../../api/indxe'
 import "./index.less";
+import dayjs from "dayjs";
 
 const { confirm } = Modal;
 class HeaderMain extends Component {
+  state = {
+    sysTime:Date.now(),
+    weather:'默认',
+    weatherImg:'http://api.map.baidu.com/images/weather/day/qing.png'
+  }
   componentWillMount () {
     this.username = getItem().username;
   }
 
+  async componentDidMount () {
+    setInterval ( () => {
+      this.setState ({
+        sysTime:Date.now()
+      })
+    },1000);
+    const result = await reqWeather();
+    
+    if (result) {
+      this.setState(result);
+    }
+  }
+  // 点击登出
   logout = () => {
     confirm({
       title: '你确定要退出登录吗?',
@@ -24,18 +43,19 @@ class HeaderMain extends Component {
     });
   }
   render() {
+    const { sysTime, weather, weatherImg } = this.state;
     return (
       <div>
         <div className="header-main-top">
-          <span>欢迎, xxx</span>
+          <span>欢迎, {this.username}</span>
           <MyButton onClick={this.logout}>退出</MyButton>
         </div>
         <div className="header-main-bottom">
           <span className="header-main-left">用户管理</span>
           <div className="header-main-right">
-            <span>{Date.now()}</span>
-            <img src={logo} alt="" />
-            <span>晴</span>
+            <span>{dayjs(sysTime).format('YYYY-MM-DD HH:mm:ss')}</span>
+            <img src={weatherImg} alt="" />
+            <span>{weather}</span>
           </div>
         </div>
       </div>
