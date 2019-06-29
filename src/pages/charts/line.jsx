de8c4dc21eb128184d713a7dcde26210d22d1267
jinhuiyong <2424960424@qq.com>
2019-06-29 10:15:59 +0800

    新增图形图表

--- src/pages/charts/line.jsx
+++ src/pages/charts/line.jsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactEcharts from "echarts-for-react";
 
 export default class Line extends Component {
+  getOption = () => {
+    return {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: "line"
+        }
+      ]
+    };
+  };
+  
   render() {
-    return <div>
-      Line
-    </div>;
+    return (
+      <div>
+        <ReactEcharts option={this.getOption()} />
+      </div>
+    );
   }
 }
--- src/pages/product/detail/index.jsx
+++ src/pages/product/detail/index.jsx
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Card, Icon, List } from "antd";
 
 export default class Detail extends Component {
+  
   render() {
-    return <div>
-      Detail
-    </div>;
+    const data = [
+      '商品名称:',
+      '商品描述:',
+      '商品价格:',
+      '商品分类',
+      '商品图片',
+      '商品详情',
+    ];
+    return (
+      <Card
+        title={
+          <div style={{ display: "flex", alignItems: "centenr" }}>
+            <Icon style={{ fontSize: 25, marginRight: 10 }} type="arrow-left" />
+            <span>商品详情</span>
+          </div>
+        }
+      >
+        <List>
+          size="large"
+          {/* header={<div>Header</div>} */}
+          {/* footer={<div>Footer</div>} */}
+          bordered
+          dataSource={data}
+          renderItem={this.renderItem}
+        </List>
+      </Card>
+    );
   }
 }
